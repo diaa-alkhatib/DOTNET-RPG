34236dd1177b60a0a4f0704f71769cec6a53fdb0
Diaa Alkhatib <alkhatib.diaaa@gmail.com>
2024-06-17 12:52:25 +0200

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DOTNET_RPG.Controllers
+{
+    [Route("[controller]")]
+    public class WeatherForecastController : Controller
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
